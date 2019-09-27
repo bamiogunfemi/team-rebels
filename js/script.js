82cde4e4ca2e70c39d32b558a820c97fc903ffc2
Midetech <faginorish@gmail.com>
2019-09-27 09:14:45 +0100

    add header component

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
 $(function(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
     var file = '../components/' + $(this).data('include') + '.html';
     $(this).load(file);
   });
 });
-=======
-    var includes = $('[data-include]');
-    jQuery.each(includes, function(){
-      var file = '../components/' + $(this).data('include') + '.html';
-      $(this).load(file);
-    });
-  });
->>>>>>> work on service page two
