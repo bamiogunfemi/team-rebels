1ba46de9a37e49d4012f77e9f1de73565c4112c5
Midetech <faginorish@gmail.com>
2019-10-02 13:36:06 +0100

    work on service page two

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(function(){
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
     var file = '../components/' + $(this).data('include') + '.html';
     $(this).load(file);
   });
 });
+=======
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+      var file = '../components/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+  });
+>>>>>>> work on service page two
